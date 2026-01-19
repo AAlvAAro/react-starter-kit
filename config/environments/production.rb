5a5719277331f128f1a9baa40a3be2e20a624f2d
Alvaro Delgado <hola@alvarodelgado.dev>
2026-01-19 03:28:20 -0600

    Update postmark key to be read from the secrets manager

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: "example.com"}
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = {
-    api_token: Rails.application.credentials.dig(:postmark, :api_token)
+    api_token: ENV["POSTMARK_API_TOKEN"]
   }
-
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
