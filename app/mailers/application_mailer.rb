eb1ab94a22c6dcd829037696f3597574ff0b6390
Alvaro Delgado <hola@alvarodelgado.dev>
2026-02-17 14:12:31 -0600

    Social Toolkit setup

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "hola@alvarodelgado.dev"
+  default from: "hey@socialtoolkit.co"
   layout "mailer"
 end
